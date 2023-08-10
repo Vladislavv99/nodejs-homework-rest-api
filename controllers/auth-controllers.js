9e07ac4e21524d960aeb29315693df07da3a6e16
Vladislavv99 <vladlegur6@gmail.com>
2023-08-10 14:36:57 +0300

    hm 6 done

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -5,24 +5,22 @@ import fs from "fs/promises";
 import path from "path";
 import gravatar from "gravatar";
 import jimp from "jimp";
+import { nanoid } from "nanoid";
 
-import { HttpError } from "../helpers/index.js";
+import { HttpError, sendEmail } from "../helpers/index.js";
 
 import User from "../models/User.js";
 
 import { ctrlWrapper } from "../decorators/index.js";
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, BASE_URL } = process.env;
 
 const avatarDir = path.resolve("public", "avatars");
-
+// !-----------------------------------------------------------------!
 const singup = async (req, res) => {
   const { email, password } = req.body;
-  ////////////////////////////////////////////////////////
 
   const avatarURL = gravatar.url(email, { s: "250" });
-
-  ////////////////////////////////////////////////////////
 
   const user = await User.findOne({ email });
   if (user) {
@@ -31,11 +29,22 @@ const singup = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
+  const verificationToken = nanoid();
+
   const newUser = await User.create({
     ...req.body,
     avatarURL,
     password: hashPassword,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a href="${BASE_URL}/api/users/verify/${verificationToken}" target="_blank">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     user: {
@@ -44,13 +53,58 @@ const singup = async (req, res) => {
     },
   });
 };
+// !-----------------------------------------------------------------!
+const verify = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw HttpError(400, "Invalid token");
+  }
 
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: "",
+  });
+
+  res.json({
+    message: "Verification successful",
+  });
+};
+// !-----------------------------------------------------------------!
+const resendVerifyEmail = async (req, res) => {
+  const { email } = res.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(400, "missing required field email");
+  }
+  if (user.verify) {
+    throw HttpError(400, "Verification has already been passed");
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a href="${BASE_URL}/api/users/verify/${user.verificationToken}" target="_blank">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.json({
+    message: "Verification email sent",
+  });
+};
+// !-----------------------------------------------------------------!
 const singin = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
   }
+
+  if (!user.verify) {
+    throw HttpError(404, "User not found");
+  }
+
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
@@ -71,7 +125,7 @@ const singin = async (req, res) => {
     },
   });
 };
-
+// !-----------------------------------------------------------------!
 const getCurrent = async (req, res) => {
   const { email, subscription } = req.user;
 
@@ -80,17 +134,16 @@ const getCurrent = async (req, res) => {
     subscription,
   });
 };
-
+// !-----------------------------------------------------------------!
 const logout = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
   res.status(204);
 };
-
+// !-----------------------------------------------------------------!
 const changeAvatar = async (req, res) => {
   const { _id } = req.user;
-  /////////////////////////////////////////////////////
 
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
@@ -103,14 +156,14 @@ const changeAvatar = async (req, res) => {
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
 
-  /////////////////////////////////////////////////////
-
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.status(200).json({ avatarURL });
 };
 
 export default {
   singup: ctrlWrapper(singup),
+  verify: ctrlWrapper(verify),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
   singin: ctrlWrapper(singin),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
